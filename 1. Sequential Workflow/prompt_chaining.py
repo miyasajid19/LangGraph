9eba2447f8f138b4bc8464b125a4c25d1319aa7f
miyasajid19 <miyasajid19@gmail.com>
2026-01-24 19:16:52 +0530

    Prompt Chaining

--- 1. Sequential Workflow/prompt_chaining.py
+++ 1. Sequential Workflow/prompt_chaining.py
@@ -34,20 +34,22 @@ graph=StateGraph(BlogState)
 
 
 def generate_outline(state:BlogState)->BlogState:
+    print("Generating outline...")
     topic=state['topic']
     prompt=f"Generate a detailed outline for a blog post about {topic}."
     response = chat.invoke([HumanMessage(content=prompt)])
     state['outline']=response.content
-    print("Generated Outline:",response.content)
-    print("==="*20)
+    print("Outline generated.")
     return state
 
 
 def generate_blog_post(state:BlogState)->BlogState:
+    print("Generating blog post...")
     outline=state['outline']
     prompt=f"Write a comprehensive blog post based on the following outline:\n{outline}"
     response = chat.invoke([HumanMessage(content=prompt)])
     state['blog_post']=response.content
+    print("Blog post generated.")
     return state
 
 
@@ -66,4 +68,14 @@ if __name__=="__main__":
     workflow=graph.compile()
     input_state={'topic':"The Future of Artificial Intelligence"}
     output_state=workflow.invoke(input_state)
-    print("Final Output State:",output_state)
+    print("\n\n\n")
+    print("Question :: ",input_state['topic'])
+    
+    
+    print("\n\n\n")
+    print("Outline :: \n")
+    print(output_state['outline'])
+    
+    print("Generated Blog Post :: \n")
+    print(output_state['blog_post'])
+    
